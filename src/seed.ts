edb7ca079d9a80c7907fb32b9157206348fb41b7
a-tione <heyanggo@gmail.com>
2021-01-01 15:10:44 +0800

    success 创建数据

--- src/seed.ts
+++ src/seed.ts
@@ -1,13 +1,26 @@
-import "reflect-metadata";
-import {createConnection} from "typeorm";
+import 'reflect-metadata';
+import {createConnection} from 'typeorm';
 import {User} from './entity/User';
+import {Post} from './entity/Post';
+import {Comment} from './entity/Comment';
 
 createConnection().then(async connection => {
-    const {manager} = connection;
-    const u1 = new User();
-    u1.username = 'tione'
-    u1.passwordDigest = 'xxxx'
-    await manager.save(u1)
-    console.log(u1.id);
-    await connection.close();
+  const {manager} = connection;
+  const u1 = new User();
+  u1.username = 'tione';
+  u1.passwordDigest = 'xxxx';
+  await manager.save(u1);
+  
+  const p1 = new Post();
+  p1.author = u1;
+  p1.title = 'tione';
+  p1.content = 'tione First Post';
+  await manager.save(p1);
+  
+  const c1 = new Comment();
+  c1.user = u1;
+  c1.post = p1;
+  c1.content = 'Awesome!';
+  await manager.save(c1);
+  await connection.close();
 }).catch(error => console.log(error));
