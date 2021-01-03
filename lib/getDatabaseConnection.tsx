7b263f170331bd6f0b329d71f5379a499ac94463
a-tione <heyanggo@gmail.com>
2021-01-03 13:24:54 +0800

    成功展示posts

--- lib/getDatabaseConnection.tsx
+++ lib/getDatabaseConnection.tsx
@@ -1,32 +1,25 @@
 import {createConnection, getConnectionManager} from 'typeorm';
-import 'reflect-metadata'
+import 'reflect-metadata';
 import {Post} from '../src/entity/Post';
 import {User} from '../src/entity/User';
 import {Comment} from '../src/entity/Comment';
-import config from 'ormconfig.json'
+import config from 'ormconfig.json';
 
 const create = async () => {
   // @ts-ignore
   return createConnection({
     ...config,
     entities: [Post, User, Comment]
-  })
-}
+  });
+};
 
 const promise = (async function () {
   const manager = getConnectionManager();
-  if (!manager.has('default')) {
-    console.log('创建connection');
-    return create();
-  } else {
-    console.log('复用connection');
-    const current = manager.get('default');
-    if (current.isConnected) {
-      return current;
-    } else {
-      return create();
-    }
+  const current = manager.has('default') && manager.get('default');
+  if (current) {
+    await current.close();
   }
+  return create();
 });
 
 export const getDatabaseConnection = async () => {
