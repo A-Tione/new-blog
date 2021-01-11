c961bbd559cc2bc639a5751a83a20478dfbb9e06
a-tione <heyanggo@gmail.com>
2021-01-11 22:15:05 +0800

    首页代码复用优化

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,44 +1,5 @@
-import {GetServerSideProps, NextPage} from 'next';
-import {UAParser} from 'ua-parser-js';
-import React, {useEffect, useState} from 'react';
-import {getDatabaseConnection} from '../lib/getDatabaseConnection';
-import {Post} from '../src/entity/Post';
-import Link from 'next/link';
+import PostsIndex, {getStaticProps} from './posts';
 
-type Props = {
-  posts: Post[],
-  browser: {
-    name: string;
-    version: string;
-    major: string;
-  }
-}
-const index: NextPage<Props> = (props) => {
-  const {posts} = props;
-  console.log(posts);
-  return (
-    <div>
-      <h1>文章列表</h1>
-      {posts.map(post =>
-        <Link key={post.id} href={`/posts/${post.id}`}>
-          <a>{post.title}</a>
-        </Link>
-      )}
-    </div>
-  );
-};
-export default index;
+export default PostsIndex;
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const connect = await getDatabaseConnection();
-  const posts = await connect.manager.find(Post);
-  console.log(posts);
-  const ua = context.req.headers['user-agent'];
-  const result = new UAParser(ua).getResult();
-  return {
-    props: {
-      browser: result.browser,
-      posts: JSON.parse(JSON.stringify(posts))
-    }
-  };
-};
+export {getStaticProps};
--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,35 +1,59 @@
-import {GetStaticProps, NextPage} from 'next';
+import {GetServerSideProps, GetStaticProps, NextPage} from 'next';
 import {getPosts} from '../../lib/posts';
 import Link from 'next/link';
-import {Post} from '../../next-env';
+import {Post} from 'src/entity/Post';
+import qs from 'querystring';
+import {getDatabaseConnection} from '../../lib/getDatabaseConnection';
+import {UAParser} from 'ua-parser-js';
 
 type Props = {
   posts: Post[];
+  count: number
+  perPage: number
+  page: number
+  totalPage: number
 }
 const PostsIndex: NextPage<Props> = (props) => {
-  const {posts} = props;
-  console.log(posts);
+  const {posts, count, page, totalPage} = props
+  const {pager} = usePager({page, totalPage})
   return (
     <div>
-      <h1>文章列表</h1>
-      {posts.map(p => <div key={p.id}>
-        <Link href={`/posts/${p.id}`}>
-          <a>
-            {p.id}
-          </a>
-        </Link>
-      </div>)}
+      <h1>文章列表({props.count}) 每页{props.perPage}</h1>
+      {posts.map(post =>
+        <div>
+          <Link href={`/posts/${post.id}`} key={post.id}>
+            <a>{post.title}</a>
+          </Link>
+        </div>
+      )}
+      <footer>
+        {pager}
+      </footer>
     </div>
-  );
+  )
 };
 
 export default PostsIndex;
 
-export const getStaticProps = async () => {
-  const posts = await getPosts();
+export const getStaticProps: GetServerSideProps = async (context) => {
+  const index = context.req.url.indexOf('?')
+  const search = context.req.url.substr(index+1)
+  const query = qs.parse(search)
+  const page = parseInt(query.page?.toString()) || 1
+  const connection = await getDatabaseConnection()
+  const perPage = 1
+  const [posts, count] = await connection.manager.findAndCount(Post,
+    {skip: (page - 1) * perPage, take: perPage})
+  console.log(posts);
+  const ua = context.req.headers['user-agent']
+  const result = new UAParser(ua).getResult()
   return {
     props: {
-      posts: JSON.parse(JSON.stringify(posts))
+      browser: result.browser,
+      posts: JSON.parse(JSON.stringify(posts)),
+      count: count,
+      perPage, page,
+      totalPage: Math.ceil(count / perPage)
     }
   };
 };
